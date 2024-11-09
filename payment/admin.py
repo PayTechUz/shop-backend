2621ab019de8922cb1094284c6f32f29bf822c43
Anvarbek <121457366+anvarbeckk@users.noreply.github.com>
2024-11-09 08:03:39 +0500

    feat: added unfold admin for better UI

---
+++ payment/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from payme.models import PaymeTransactions
+from unfold.admin import ModelAdmin
+
+admin.site.unregister(PaymeTransactions)
+
+
+@admin.register(PaymeTransactions)
+class PaymeTransactionsAdmin(ModelAdmin):
+    """
+    Admin for PaymeTransactions with display, search, and filter options.
+    """
+    list_display = ['transaction_id', 'state', 'created_at']
+    search_fields = ['transaction_id']
+    list_filter = ['state']
