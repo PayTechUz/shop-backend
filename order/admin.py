baac9c6430609be0d1d2d29fa07d061eac904c2a
Anvarbek <121457366+anvarbeckk@users.noreply.github.com>
2024-11-09 08:23:01 +0500

    small changes

--- order/admin.py
+++ order/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
-from django.contrib.auth.models import User, Group
 
 from unfold.admin import ModelAdmin
+
 from order.models import Order
 
 admin.site.unregister(User)
--- payment/admin.py
+++ payment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from payme.models import PaymeTransactions
+
 from unfold.admin import ModelAdmin
+
+from payme.models import PaymeTransactions
 
 admin.site.unregister(PaymeTransactions)
 
